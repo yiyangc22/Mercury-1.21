d635b61a1ecbc783fdd8fe0a443886bef627f3d7
Yiyang (Ian) Chen <160797348+yiyangc22@users.noreply.github.com>
2024-10-19 12:44:45 -0700

    Add files via upload
    File for testing Mercury I-III without LabVIEW.

---
+++ _main.py
@@ -0,0 +1,12 @@
+"""
+Mercury 00: python program mainframe, project version 1.2 (with python 3.9).
+"""
+
+# from mercury_01 import mercury_01
+# print(mercury_01())
+
+# from mercury_02 import mercury_02
+# print(mercury_02())
+
+# from mercury_03 import mercury_03
+# print(mercury_03())
